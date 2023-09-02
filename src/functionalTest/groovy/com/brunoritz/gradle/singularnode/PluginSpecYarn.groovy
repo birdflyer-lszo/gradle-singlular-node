06f3d765618c16d9af16c3f664f396651ca2dfa6
Ritz, Bruno <Bruno.Ritz@zuehlke.com>
2023-09-02 23:18:13 +0200

    refactor: Minor cleanups

--- src/functionalTest/groovy/com/brunoritz/gradle/singularnode/PluginSpecYarn.groovy
+++ src/functionalTest/groovy/com/brunoritz/gradle/singularnode/PluginSpecYarn.groovy
@@ -8,11 +8,11 @@ import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
 class PluginSpecYarn
 	extends Specification
 {
-	def rootProjectDir
-	def subProjectDir
-	def rootBuildFile
-	def subProjectBuildFile
-	def settingsFile
+	private File rootProjectDir
+	private File subProjectDir
+	private File rootBuildFile
+	private File subProjectBuildFile
+	private File settingsFile
 
 	def setup()
 	{
@@ -38,6 +38,11 @@ class PluginSpecYarn
 		settingsFile << '''
 				include ':subproject'
 			'''
+	}
+
+	def cleanup()
+	{
+		rootProjectDir.deleteOnExit()
 	}
 
 	def 'It shall be possible to run Yarn tasks'()
--- src/functionalTest/groovy/com/brunoritz/gradle/singularnode/PuginSpecNpm.groovy
+++ src/functionalTest/groovy/com/brunoritz/gradle/singularnode/PuginSpecNpm.groovy
@@ -8,11 +8,11 @@ import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
 class PuginSpecNpm
 	extends Specification
 {
-	def rootProjectDir
-	def subProjectDir
-	def rootBuildFile
-	def subProjectBuildFile
-	def settingsFile
+	private File rootProjectDir
+	private File subProjectDir
+	private File rootBuildFile
+	private File subProjectBuildFile
+	private File settingsFile
 
 	def setup()
 	{
@@ -38,6 +38,11 @@ class PuginSpecNpm
 		settingsFile << '''
 				include ':subproject'
 			'''
+	}
+
+	def cleanup()
+	{
+		rootProjectDir.deleteOnExit()
 	}
 
 	def 'It shall be possible to run NPM tasks'()
